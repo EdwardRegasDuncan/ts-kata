4d55b92f7f7b88f1bae5e5cc3b93b3d7627a26ed
Edward <edward.regasduncan@vista.com>
2023-05-15 16:54:17 +0200

    refactor: extract methods

--- src/10_Tennis/kata.ts
+++ src/10_Tennis/kata.ts
@@ -24,51 +24,50 @@ export class TennisGame1 implements TennisGame {
   }
 
   getScore(): string {
-    let score = '';
-    let tempScore = 0;
-
-    if (this.m_score1 === this.m_score2) 
-    {
-      score = this.translateScore(score);
-    } 
-    else if (this.m_score1 >= 4 || this.m_score2 >= 4) 
-    {
-      const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = 'Advantage player1';
-      else if (minusResult === -1) score = 'Advantage player2';
-      else if (minusResult >= 2) score = 'Win for player1';
-      else score = 'Win for player2';
-    } 
-    else 
-    {
-      for (let i = 1; i < 3; i++) 
-      {
-        if (i === 1) 
-        {
-          tempScore = this.m_score1;
-        }
-        else 
-        {
-          score += '-';
-          tempScore = this.m_score2;
-        }
-        switch (tempScore) {
-          case 0:
-            score += 'Love';
-            break;
-          case 1:
-            score += 'Fifteen';
-            break;
-          case 2:
-            score += 'Thirty';
-            break;
-          case 3:
-            score += 'Forty';
-            break;
-        }
-      }
+    if (this.m_score1 === this.m_score2) {
+      return this.translateScore('');
     }
 
+    if (this.m_score1 >= 4 || this.m_score2 >= 4) {
+      return this.translateScore2('');
+    }
+
+    return this.newMethod('');
+  }
+
+  private newMethod(score: string) {
+    let tempScore = 0;
+    for (let i = 1; i < 3; i++) {
+      if (i === 1) {
+        tempScore = this.m_score1;
+      } else {
+        score += '-';
+        tempScore = this.m_score2;
+      }
+      switch (tempScore) {
+        case 0:
+          score += 'Love';
+          break;
+        case 1:
+          score += 'Fifteen';
+          break;
+        case 2:
+          score += 'Thirty';
+          break;
+        case 3:
+          score += 'Forty';
+          break;
+      }
+    }
+    return score;
+  }
+
+  private translateScore2(score: string) {
+    const minusResult: number = this.m_score1 - this.m_score2;
+    if (minusResult === 1) score = 'Advantage player1';
+    else if (minusResult === -1) score = 'Advantage player2';
+    else if (minusResult >= 2) score = 'Win for player1';
+    else score = 'Win for player2';
     return score;
   }
 
